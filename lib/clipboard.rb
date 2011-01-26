5fa5ae0fe813b4ecfbce8bee706e577b02d78b24
grosser <grosser.michael@gmail.com>
2011-01-26 12:40:18 +0100

    simplify loading

--- lib/clipboard.rb
+++ lib/clipboard.rb
@@ -2,17 +2,15 @@ module Clipboard
   extend self
   VERSION = File.read( (File.dirname(__FILE__) + '/../VERSION') ).chomp
 
-  os = proc do
-    case RbConfig::CONFIG['host_os']
-    when /mac|darwin/ then 'mac'
-    when /linux|cygwin/ then 'linux'
-    when /mswin|mingw/ then 'windows'
-    when /bsd/ then 'linux'
-    # when /solaris|sunos/ then 'linux' # needs testing..
-    else
-      raise "You OS is not supported -> fork me"
-    end
+  os = case RbConfig::CONFIG['host_os']
+  when /mac|darwin/ then 'mac'
+  when /linux|cygwin/ then 'linux'
+  when /mswin|mingw/ then 'windows'
+  when /bsd/ then 'linux'
+  # when /solaris|sunos/ then 'linux' # needs testing..
+  else
+    raise "You OS is not supported -> fork me"
   end
 
-  require "clipboard/#{os.call}"
+  require "clipboard/#{os}"
 end
