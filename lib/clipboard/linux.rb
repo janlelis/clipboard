16f44333fce9d62c2c835245f1c9e3c296c7f93e
Jan Lelis <mail@janlelis.de>
2011-01-28 18:22:49 +0100

    also allow xsel for linux

--- lib/clipboard/linux.rb
+++ lib/clipboard/linux.rb
@@ -6,19 +6,27 @@ module Clipboard::Linux
   extend self
 
   CLIPBOARDS   = %w[clipboard primary secondary]
-  WriteCommands = CLIPBOARDS.map{|cb| 'xclip -selection ' + cb }
-  ReadCommand  = 'xclip -o'
 
-  #catch dependency errors
-  if not `which xclip`
+  # check which backend to use
+  if !`which xclip`.empty?
+    WriteCommand = 'xclip'
+    ReadCommand  = 'xclip -o'
+    Selection    = proc{|x| "-selection #{x}"}
+  elsif !`which xsel`.empty?
+    WriteCommand = 'xsel'
+    ReadCommand  = 'xsel -o'
+    Selection    = {'clipboard' => '-b', 'primary' => '-p', 'secondary' => '-s'}
+  else
     raise Clipboard::ClipboardLoadError, "clipboard:\n" \
-          "Could not find required program xclip\n" \
+          "Could not find required program xclip (or xsel)\n" \
           "On debian/ubuntu, you can install it with: sudo apt-get install xclip"
   end
 
   def paste(which = nil)
-    which ||= CLIPBOARDS.first
-    `#{ReadCommand} -selection #{which}`
+    if !which || !CLIPBOARDS.include?(which.to_s.downcase)
+      which = CLIPBOARDS.first
+    end
+    `#{ReadCommand} #{Selection[which.to_s.downcase]}`
   end
 
   def clear
@@ -26,8 +34,8 @@ module Clipboard::Linux
   end
 
   def copy(data)
-    WriteCommands.each{ |cmd|
-      Open3.popen3( cmd ){ |input,_,_| input << data }
+    CLIPBOARDS.each{ |which|
+      Open3.popen3( "#{WriteCommand} #{Selection[which.to_s.downcase]}" ){ |input,_,_| input << data }
     }
     paste
   end
