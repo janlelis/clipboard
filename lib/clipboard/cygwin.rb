da8773987b2e494415ec41c968e149684b10707b
Jan Lelis <mail@janlelis.de>
2016-03-14 12:39:39 +0100

    Working Cygwin

--- lib/clipboard/cygwin.rb
+++ lib/clipboard/cygwin.rb
@@ -1,16 +1,14 @@
-require 'open3'
-
 module Clipboard; end
 
 module Clipboard::Cygwin
   extend self
 
   def paste(_ = nil)
-    print File.read('/dev/clipboard')
+    File.read("/dev/clipboard")
   end
 
   def copy(data)
-    Open3.popen3('/dev/clipboard'){ |input,_,_| input << data }
+    File.open("/dev/clipboard", "w"){ |f| f.write(data) }
     paste
   end
 
