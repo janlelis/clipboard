98d50df52812c406a0e8e7aa5a50b3b32188b83c
Pavel Lobashov <shockwavenn@gmail.com>
2016-05-11 19:56:21 +0300

    Fix rubocop Style/ConstantName cop

--- lib/clipboard/linux.rb
+++ lib/clipboard/linux.rb
@@ -9,13 +9,13 @@ module Clipboard::Linux
 
   # check which backend to use
   if system('which xclip >/dev/null 2>&1')
-    WriteCommand = 'xclip'
-    ReadCommand  = 'xclip -o'
-    Selection    = proc{|x| "-selection #{x}"}
+    WRITE_COMMAND = 'xclip'
+    READ_COMMAND  = 'xclip -o'
+    SELECTION    = proc{|x| "-selection #{x}"}
   elsif system('which xsel >/dev/null 2>&1')
-    WriteCommand = 'xsel -i'
-    ReadCommand  = 'xsel -o'
-    Selection    = {'clipboard' => '-b', 'primary' => '-p', 'secondary' => '-s'}
+    WRITE_COMMAND = 'xsel -i'
+    READ_COMMAND  = 'xsel -o'
+    SELECTION    = {'clipboard' => '-b', 'primary' => '-p', 'secondary' => '-s'}
   else
     raise Clipboard::ClipboardLoadError, "clipboard: Could not find required program xclip or xsel\n" \
           'On debian/ubuntu, you can install it with: sudo apt-get install xclip'
@@ -25,7 +25,7 @@ module Clipboard::Linux
     if !which || !CLIPBOARDS.include?(which.to_s.downcase)
       which = CLIPBOARDS.first
     end
-    `#{ReadCommand} #{Selection[which.to_s.downcase]}`
+    `#{READ_COMMAND} #{SELECTION[which.to_s.downcase]}`
   end
 
   def clear
@@ -34,7 +34,7 @@ module Clipboard::Linux
 
   def copy(data)
     CLIPBOARDS.each{ |which|
-      Open3.popen3("#{WriteCommand} #{Selection[which.to_s.downcase]}"){ |input,_,_| input << data }
+      Open3.popen3("#{WRITE_COMMAND} #{SELECTION[which.to_s.downcase]}"){ |input,_,_| input << data }
     }
     paste
   end
