782d62add35efecd22692073cdfafdd12f566ff5
Pavel Lobashov <shockwavenn@gmail.com>
2016-05-13 19:03:14 +0300

    Fix rubocop Style/SpaceAroundOperators cop

--- lib/clipboard/linux.rb
+++ lib/clipboard/linux.rb
@@ -5,17 +5,17 @@ module Clipboard; end
 module Clipboard::Linux
   module_function
 
-  CLIPBOARDS   = %w[clipboard primary secondary]
+  CLIPBOARDS = %w[clipboard primary secondary]
 
   # check which backend to use
   if system('which xclip >/dev/null 2>&1')
     WRITE_COMMAND = 'xclip'
     READ_COMMAND  = 'xclip -o'
-    SELECTION    = proc { |x| "-selection #{x}" }
+    SELECTION = proc { |x| "-selection #{x}" }
   elsif system('which xsel >/dev/null 2>&1')
     WRITE_COMMAND = 'xsel -i'
     READ_COMMAND  = 'xsel -o'
-    SELECTION    = { 'clipboard' => '-b', 'primary' => '-p', 'secondary' => '-s' }
+    SELECTION = { 'clipboard' => '-b', 'primary' => '-p', 'secondary' => '-s' }
   else
     raise Clipboard::ClipboardLoadError, "clipboard: Could not find required program xclip or xsel\n" \
           'On debian/ubuntu, you can install it with: sudo apt-get install xclip'
