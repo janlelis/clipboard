b72529a5b89913a5cd8a28fe3214bd486285884e
Pavel Lobashov <shockwavenn@gmail.com>
2016-05-13 19:04:51 +0300

    Fix rubocop Style/RescueModifier cop

--- lib/clipboard/file.rb
+++ lib/clipboard/file.rb
@@ -6,12 +6,18 @@ module Clipboard::File
   FILE = File.expand_path('~/.clipboard')
 
   def copy(text)
-    File.open(FILE, 'w', 0600) { |f| f.write(text) } rescue ''
+    begin
+      File.open(FILE, 'w', 0600) { |f| f.write(text) }
+    rescue
+      ''
+    end
     paste
   end
 
   def paste(_ = nil)
-    File.read(FILE) rescue ''
+    File.read(FILE)
+  rescue
+    ''
   end
 
   def clear
