8515d5df2ea5346d000053e30f763878235218fa
Jan Lelis <hi@syntaxtransformation.com>
2024-01-06 17:02:23 +0100

    Wayland: Raise error when wl-copy executable cannot be found

--- lib/clipboard/linux_wayland.rb
+++ lib/clipboard/linux_wayland.rb
@@ -6,17 +6,25 @@ module Clipboard
   module LinuxWayland
     extend self
 
+    WRITE_COMMAND = "wl-copy"
+    READ_COMMAND = "wl-paste"
+
+    if !Utils.executable_installed?(WRITE_COMMAND)
+      raise Clipboard::ClipboardLoadError, "clipboard: Could not find required program wl-copy\n" \
+                                           "Please install it or try a different implementation"
+    end
+
     def paste(_ = nil)
-      `wl-paste`
+      `#{READ_COMMAND}`
     end
 
     def copy(data)
-      Utils.popen "wl-copy", data
+      Utils.popen WRITE_COMMAND, data
       paste
     end
 
     def clear
-      `wl-copy -c`
+      `#{WRITE_COMMAND} -c`
     end
   end
 end
