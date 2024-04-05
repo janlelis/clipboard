ae3ecb140bd187710ca060bdc738546a4fa28451
Jan Lelis <hi@syntaxtransformation.com>
2024-04-05 22:26:20 +0200

    Add spec for Clipboard::Java

---
+++ spec/clipboard_java_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative "spec_helper"
+
+if RUBY_ENGINE === "jruby"
+  require 'clipboard/java'
+
+  describe "Clipboard::Java" do
+    before :all do
+      Clipboard.implementation = Clipboard::Java
+    end
+
+    it "can copy & paste" do
+      expect( Clipboard.copy('example') ).to eq true
+      expect( Clipboard.paste ).to eq 'example'
+    end
+
+    it "can clear" do
+      expect( Clipboard.copy('example') ).to eq true
+      expect( Clipboard.paste ).to eq 'example'
+      Clipboard.clear
+      expect( Clipboard.paste ).to eq ''
+    end
+  end
+end
