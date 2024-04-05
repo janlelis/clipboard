c3ca46f15f004701770e873205d13b25fefd5b12
Jan Lelis <hi@syntaxtransformation.com>
2024-04-05 20:58:01 +0200

    Add more info to base module

--- lib/clipboard/implementation.rb
+++ lib/clipboard/implementation.rb
@@ -4,16 +4,24 @@ module Clipboard
   module Implementation
     extend self
 
+    # Implement paste
+    # Should take an optional argument
     def paste(_clipboard_name = nil)
       raise NotImplementedError, "paste not supported by implementation"
     end
 
+    # Takes the data to copy as argument
+    # Should return true
     def copy(_data)
       raise NotImplementedError, "copy not supported by implementation"
     end
 
+    # Can be used to add a native clear implementation
+    # Should return true
     def clear
       copy ''
+
+      true
     end
   end
 end
